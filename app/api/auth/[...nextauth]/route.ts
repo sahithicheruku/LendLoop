876fd4d77ea7f7b2d6f718798f1142a71cc11a33
sahithicheruku <100852983+sahithicheruku@users.noreply.github.com>
2026-02-21 07:51:26 -0600

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,32 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: "jwt",
-  },
+
   secret: process.env.NEXTAUTH_SECRET,
+
   pages: {
-  signIn: "/signin",
-},
+    signIn: "/signin",
+  },
+
+  callbacks: {
+    async session({ session, user }) {
+      // add role to session
+      (session.user as any).role = (user as any).role;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
+
